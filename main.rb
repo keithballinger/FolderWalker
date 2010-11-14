b2e950f90f8bfcede528c749de52109bb7d7da5a
Keith Ballinger <KeithBa+git@gmail.com>
2010-11-14 14:47:51 -0500

    1) typo in requires 2) additional adhoc testing of report methods

--- main.rb
+++ main.rb
@@ -6,13 +6,15 @@
 # License::   Distributes under the same terms as Ruby
 
 require "./FolderWalker"
-require "./ConsoleReporter"
+require "./Reporter"
 
 if __FILE__ == $0
 	f = FolderWalker.new("C:/users/keith/downloads")
 	f.run
-	r = ConsoleReporter.new(f)
-	r.print_tree_size()
-	#f.print_file_sizes
-	#f.print_folder_sizes
+	r = Reporter.new(f)
+	r.print_tree_size
+	r.print_file_sizes
+	r.print_folder_sizes
+	r.print_file_sizes(Format::CSV)
+	r.print_folder_sizes(Format::CSV)
 end
